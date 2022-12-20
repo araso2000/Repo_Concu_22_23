7d42edcf1efef2f9d0b88628a6730edf1b8d2383
Álvaro Raso Pasero <a.raso.pasero@gmail.com>
2022-12-20 18:51:38 +0100

    20.12.2022_1

--- Ejes_Concu/src/main/java/misEjes_Parte2/Eje8.java
+++ Ejes_Concu/src/main/java/misEjes_Parte2/Eje8.java
@@ -11,47 +11,83 @@ public class Eje8 {
 	private int nextOUTPos = 0;
 	private int posDisponibles = 10;
 	
-	Lock lockCond_1 = new ReentrantLock();
-	Condition cd_1 = lockCond_1.newCondition();
+	static Lock cerrojo = new ReentrantLock();
+	static Condition cd1 = cerrojo.newCondition();
+	static Condition cd2 = cerrojo.newCondition();
 	
-	Lock lock1 = new ReentrantLock();
-	
-	public void insertar(int dato) {
-		try {
-			lock1.lock();
-			
-			while(posDisponibles == 0) {
-				try {
-					cd_1.await();
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			
-		} finally{
-			
-		}
+	public void insertar(int dato) throws InterruptedException {
+		cerrojo.lock();
 		
+		while(posDisponibles == 0) {
+			cd1.await();
+		}
+			
 		array[nextINPos] = dato;
 		nextINPos = (nextINPos+1) % array.length;
+		posDisponibles--;
+		
+		cd2.signal();
+		
+		cerrojo.lock();
 	}
 	
-	public int sacar() {
+	public int sacar() throws InterruptedException{
+		cerrojo.lock();
+		
+		while(posDisponibles == 10) {
+			cd2.await();
+		}
+		
 		int returnear = array[nextOUTPos];
 		nextOUTPos = (nextOUTPos+1) % array.length;
+		posDisponibles++;
+		
+		cd1.signal();
+		
+		cerrojo.unlock();
+		
 		return returnear;
 	}
 	
+	
 	public void productor() {
-		insertar((int)(Math.random()*50));
+		try {
+			while(true) {
+				int numero = (int)(Math.random()*50);
+				insertar(numero);
+				System.out.println(Thread.currentThread().getName() + ": Producido: " + numero);
+				Thread.sleep(1000);
+			}
+			
+		} catch (InterruptedException e) {
+			System.out.println(Thread.currentThread().getName() + ": No se ha podido producir");
+		}
 	}
 	
 	public void consumidor() {
-		System.out.println("Consumido: " + sacar());
+		try {
+			while(true) {
+				int numero = sacar();
+				Thread.sleep(1000);
+				System.out.println(Thread.currentThread().getName() + ": Consumido: " + numero);
+			}
+			
+		} catch (InterruptedException e) {
+			System.out.println(Thread.currentThread().getName() + ": No se ha podido consumir");
+		}
+	}
+	
+	public void exec() {
+		for (int i = 0; i < 5; i++) {
+			new Thread(()->productor(),"++Productor  " + i).start();
+		}
+
+		for (int i = 0; i < 5; i++) {
+			new Thread(()->consumidor(),"--Consumidor " + i).start();
+		}
 	}
 
 	public static void main(String[] args) {
-
+		new Eje8().exec();
 	}
 }
---
+++ Ejes_Concu/src/main/java/misEjes_Parte2/Eje9.java
@@ -0,0 +1,102 @@
+package misEjes_Parte2;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Eje9 {
+	
+	private static final int N_ESCRITOR = 3;
+	private static final int N_LECTOR = 5;
+	
+	static Lock cerrojo = new ReentrantLock();
+	
+	static Condition condLector = cerrojo.newCondition();
+	static Condition condEscritor = cerrojo.newCondition();
+	
+	private static volatile int numBarreraLector,numBarreraEscritor;
+	
+	
+	public void lector() throws InterruptedException {
+		while(true) {
+			cerrojo.lock();
+			
+			System.out.println(Thread.currentThread().getName() + "_Leyendo...");
+			Thread.sleep(2000);
+			
+			numBarreraLector++;
+			
+			System.out.println(Thread.currentThread().getName() + "_Leido");
+			
+			if(numBarreraLector < N_LECTOR) {
+				while(numBarreraLector < N_LECTOR) {
+					condLector.await();
+				}
+			}else {
+				condLector.signalAll();
+				numBarreraLector = 0;
+			}
+			
+			cerrojo.unlock();
+			
+			System.out.println(Thread.currentThread().getName() + "_Procesar datos");
+		}
+	}
+	
+	public void escritor() throws InterruptedException {
+		while(true) {
+			System.out.println(Thread.currentThread().getName() + "_Generando...");
+			
+			cerrojo.lock();
+			
+			Thread.sleep(2000);
+			
+			numBarreraEscritor++;
+			
+			System.out.println(Thread.currentThread().getName() + "_Generado->Escribir datos");
+			
+			if(numBarreraEscritor < N_ESCRITOR) {
+				while(numBarreraEscritor < N_ESCRITOR) {
+					condEscritor.await();
+				}
+			}else {
+				condEscritor.signalAll();
+				numBarreraEscritor = 0;
+			}
+			
+			cerrojo.unlock();
+			
+			System.out.println(Thread.currentThread().getName() + "_Escrito");
+		}
+	}
+	
+	public void exec() {
+		numBarreraLector = 0;
+		numBarreraEscritor = 0;
+		
+		for(int ii=0;ii<N_LECTOR;ii++) {
+			new Thread(() -> {
+				try {
+					lector();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}, "lector" + ii).start();
+		}
+		
+		for(int ii=0;ii<N_ESCRITOR;ii++) {
+			new Thread(() -> {
+				try {
+					escritor();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}, "escritor" + ii).start();
+		}
+	}
+
+	public static void main(String[] args) {
+		new Eje9().exec();
+	}
+
+}
